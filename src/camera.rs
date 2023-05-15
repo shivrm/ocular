de05580a6875d38d5c9da6b87f176d068a94c90f
shivrm <shivrm@proton.me>
2023-05-15 21:12:34 +0530

    Implement rendering to BMP image

--- src/camera.rs
+++ src/camera.rs
@@ -2,7 +2,7 @@ use super::{Point, Ray, Vec3};
 
 pub struct Camera {
     origin: Point,
-    screen_bottom_left: Point,
+    screen_top_left: Point,
     horizontal: Vec3,
     vertical: Vec3,
 }
@@ -12,20 +12,19 @@ impl Camera {
         let horizontal = Vec3::new(vw, 0.0, 0.0);
         let vertical = Vec3::new(0.0, vh, 0.0);
 
-        let screen_bottom_left =
-            origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
+        let screen_top_left =
+            origin - (horizontal / 2.0) + (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
 
         Self {
             origin,
-            screen_bottom_left,
+            screen_top_left,
             horizontal,
             vertical,
         }
     }
 
     pub fn ray(&self, frac_x: f32, frac_y: f32) -> Ray {
-        let target =
-            self.screen_bottom_left + (self.horizontal * frac_x) + (self.vertical * frac_y);
+        let target = self.screen_top_left + (self.horizontal * frac_x) - (self.vertical * frac_y);
 
         Ray::new(self.origin, target - self.origin)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use ocular::{Camera, Point, Scene};
 
+const WIDTH: usize = 64;
+const HEIGHT: usize = 36;
+
 fn main() {
     let camera = Camera::new(Point::new(0.0, 0.0, 0.0), 3.55, 2.0, 1.0);
     let scene = Scene::new(camera);
-    let image = scene.render(64, 36);
+    let image = scene.render(WIDTH, HEIGHT);
+
+    let mut bitmap = bmp::Image::new(WIDTH as u32, HEIGHT as u32);
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let ocular::Pixel { r, g, b } = image.get_pixel(x, y);
+            bitmap.set_pixel(x as u32, y as u32, bmp::Pixel { r, g, b });
+        }
+    }
+
+    bitmap.save("result.bmp").unwrap();
 }
