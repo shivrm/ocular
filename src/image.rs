c48e24d897aee4b93613d51a625440f4b855fd60
shivrm <shivrm@proton.me>
2023-05-15 13:19:22 +0530

    Add Pixel and Image structs

---
+++ src/image.rs
@@ -0,0 +1,53 @@
+use super::Color;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Pixel {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Pixel {
+    pub fn from_color(color: Color) -> Pixel {
+        let color = color * 255.999;
+        Pixel {
+            r: color.x as u8,
+            g: color.y as u8,
+            b: color.z as u8,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Image {
+    width: usize,
+    height: usize,
+    data: Vec<Pixel>,
+}
+
+impl Image {
+    pub fn new(width: usize, height: usize) -> Self {
+        let mut data = Vec::with_capacity(width * height);
+        for _ in 0..(width * height) {
+            data.push(Pixel { r: 0, g: 0, b: 0 })
+        }
+
+        Self {
+            width,
+            height,
+            data,
+        }
+    }
+
+    pub fn get_pixel(&self, x: usize, y: usize) -> Pixel {
+        assert!(y < self.height);
+        assert!(x < self.width);
+        self.data[y * self.width + x]
+    }
+
+    pub fn set_pixel(&mut self, x: usize, y: usize, pixel: Pixel) {
+        assert!(y < self.height);
+        assert!(x < self.width);
+        self.data[y * self.width + x] = pixel;
+    }
+}
--- src/vec3.rs
+++ src/vec3.rs
@@ -2,9 +2,9 @@ use core::ops::{Add, Div, Mul, Neg, Sub};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vec3 {
