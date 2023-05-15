0010c90b4f76e48558327472d1a0f53f7eb44f0e
shivrm <shivrm@proton.me>
2023-05-15 12:44:16 +0530

    Add Ray and Camera structs

---
+++ src/ray.rs
@@ -0,0 +1,17 @@
+use super::{Point, Vec3};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    origin: Point,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub const fn new(origin: Point, direction: Vec3) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn at(&self, t: f32) -> Point {
+        self.origin + t * self.direction
+    }
+}
